2c3710ccae3756a1532aa022998b265067fd8b8a
lindsfund <71404773+lindsfund@users.noreply.github.com>
2023-11-11 10:15:24 -0500

    Final commit

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -24,7 +24,7 @@ public class Movie
     [Column(TypeName = "decimal(18,2)")]
     public decimal Price { get; set; }
 
-    [RegularExpression(@"^[A_Z]+[a-zA-Z0-9""'\s-]*$")]
+    [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
     [StringLength(5)]
     [Required]
     public string? Rating { get; set; }
